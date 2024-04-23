8fd6378da15b4ee50e82d4fdc62cc156e3a8dfba
Nicolás Guzmán <n3kguzman@gmail.com>
2024-04-23 12:27:26 -0400

    creando componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Form from './componentes/form'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Form />
     </>
   )
 }
---
+++ src/componentes/form.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div className='container-fluid'>
+            <div className='row' >
+                <div className='col-md-12 d-flex justify-content-center'>
+            <h1>Add New Contact</h1>
+            </div>
+            </div>
+            <form className=''>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Full Name</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Full Name" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter Email" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Phone</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter Phone" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter Address" aria-describedby="emailHelp"/>
+                </div>
+                <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
